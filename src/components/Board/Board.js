226d8e230dc4009ff4e7cf5d2f7e5e958a74799f
Kyle Lane <kyle_lane19@hotmail.ca>
2019-02-10 11:55:28 -0500

    Fixed issues with previous commit fixes

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -24,57 +24,9 @@ class Board extends React.Component {
     this.state = {
       values: new Array(9).fill(-1),
       message: "Click any box to start!!",
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       oTurn: true,
       winner: -1,
       turnNum: 1,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      xTurn: true,
-=======
-      oTurn: true,
->>>>>>> cf218b0... Added turn tracking.
-      winner: -1,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
-=======
-      xTurn: true,
-=======
-      oTurn: true,
->>>>>>> cf218b0... Added turn tracking.
-      winner: -1,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
-=======
-      xTurn: true,
-=======
-      oTurn: true,
->>>>>>> cf218b0... Added turn tracking.
-      winner: -1,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
-=======
-      xTurn: true,
-=======
-      oTurn: true,
->>>>>>> cf218b0... Added turn tracking.
-      winner: -1,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
     };
     this.restart = this.restart.bind(this);
     this.winnerMessage = this.winnerMessage.bind(this);
@@ -86,85 +38,12 @@ class Board extends React.Component {
       return(false);
     } else {
       let newValues = this.state.values;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       this.state.oTurn ? newValues[index] = 2 : newValues[index] = 1;
       this.setState({
         values: newValues,
         turnNum: this.state.turnNum+1,
         oTurn: !this.state.oTurn,
       });
-<<<<<<< HEAD
-=======
-      this.state.xTurn ? newValues[index] = 2 : newValues[index] = 1;
-      this.setState({ values: newValues});
-      this.setState({ xTurn: !this.state.xTurn });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      this.state.oTurn ? newValues[index] = 2 : newValues[index] = 1;
-<<<<<<< HEAD
-      this.setState({ values: newValues});
-      this.setState({ oTurn: !this.state.oTurn });
->>>>>>> cf218b0... Added turn tracking.
-=======
-      this.setState({
-        values: newValues,
-        turnNum: this.state.turnNum+1,
-        oTurn: !this.state.oTurn,
-      });
->>>>>>> c612210... Added turn number
-=======
-      this.state.xTurn ? newValues[index] = 2 : newValues[index] = 1;
-      this.setState({ values: newValues});
-      this.setState({ xTurn: !this.state.xTurn });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      this.state.oTurn ? newValues[index] = 2 : newValues[index] = 1;
-<<<<<<< HEAD
-      this.setState({ values: newValues});
-      this.setState({ oTurn: !this.state.oTurn });
->>>>>>> cf218b0... Added turn tracking.
-=======
->>>>>>> c612210... Added turn number
-=======
-      this.state.xTurn ? newValues[index] = 2 : newValues[index] = 1;
-      this.setState({ values: newValues});
-      this.setState({ xTurn: !this.state.xTurn });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      this.state.oTurn ? newValues[index] = 2 : newValues[index] = 1;
-<<<<<<< HEAD
-      this.setState({ values: newValues});
-      this.setState({ oTurn: !this.state.oTurn });
->>>>>>> cf218b0... Added turn tracking.
-=======
-=======
->>>>>>> c612210... Added turn number
-      this.setState({
-        values: newValues,
-        turnNum: this.state.turnNum+1,
-        oTurn: !this.state.oTurn,
-      });
-<<<<<<< HEAD
->>>>>>> c612210... Added turn number
-=======
-      this.state.xTurn ? newValues[index] = 2 : newValues[index] = 1;
-      this.setState({ values: newValues});
-      this.setState({ xTurn: !this.state.xTurn });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      this.state.oTurn ? newValues[index] = 2 : newValues[index] = 1;
-      this.setState({ values: newValues});
-      this.setState({ oTurn: !this.state.oTurn });
->>>>>>> cf218b0... Added turn tracking.
-=======
->>>>>>> c612210... Added turn number
       return(true);
     }
   }
@@ -184,54 +63,10 @@ class Board extends React.Component {
     for(let i = 0; i<wins.length; i++){
       const winCombo = wins[i];
       if((v[winCombo[0]] !== -1) && (v[winCombo[0]] === v[winCombo[1]] && v[winCombo[0]] === v[winCombo[2]])) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c612210... Added turn number
-=======
->>>>>>> c612210... Added turn number
-=======
->>>>>>> c612210... Added turn number
-=======
->>>>>>> c612210... Added turn number
         this.setState({
           message: "There is a winner!!!!",
           winner: v[winCombo[0]]
         });
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        this.setState({ message: "There is a winner!!!!"});
-        this.setState({ winner: v[winCombo[0]] });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
-=======
-        this.setState({ message: "There is a winner!!!!"});
-        this.setState({ winner: v[winCombo[0]] });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
-=======
-        this.setState({ message: "There is a winner!!!!"});
-        this.setState({ winner: v[winCombo[0]] });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
-=======
-        this.setState({ message: "There is a winner!!!!"});
-        this.setState({ winner: v[winCombo[0]] });
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
->>>>>>> c612210... Added turn number
       }
     }
   }
@@ -257,55 +92,8 @@ class Board extends React.Component {
       message: "Game has been restarted",
       values: new Array(9).fill(-1),
       winner: -1,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       oTurn: true,
       turnNum: 1
-<<<<<<< HEAD
-=======
-      xTurn: true,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      oTurn: true,
-<<<<<<< HEAD
->>>>>>> cf218b0... Added turn tracking.
-=======
-      turnNum: 1
->>>>>>> c612210... Added turn number
-=======
-      xTurn: true,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      oTurn: true,
-<<<<<<< HEAD
->>>>>>> cf218b0... Added turn tracking.
-=======
->>>>>>> c612210... Added turn number
-=======
-      xTurn: true,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      oTurn: true,
-<<<<<<< HEAD
->>>>>>> cf218b0... Added turn tracking.
-=======
-      turnNum: 1
->>>>>>> c612210... Added turn number
-=======
-      xTurn: true,
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-      oTurn: true,
->>>>>>> cf218b0... Added turn tracking.
-=======
-      turnNum: 1
->>>>>>> c612210... Added turn number
     })
   }
 
@@ -315,56 +103,8 @@ class Board extends React.Component {
       <React.Fragment>
         <h1>{this.state.message}</h1>
         <h2>{this.winnerMessage()}</h2>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         <h2>To Play: {this.state.oTurn ? "o" : "x"}</h2>
         <h2>Turn Number: {this.state.turnNum}</h2>
-=======
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-        <h2>Turn: {this.state.oTurn ? "o" : "x"}</h2>
->>>>>>> cf218b0... Added turn tracking.
-=======
-        <h2>To Play: {this.state.oTurn ? "o" : "x"}</h2>
-        <h2>Turn Number: {this.state.turnNum}</h2>
->>>>>>> c612210... Added turn number
-=======
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-        <h2>Turn: {this.state.oTurn ? "o" : "x"}</h2>
->>>>>>> cf218b0... Added turn tracking.
-=======
-        <h2>To Play: {this.state.oTurn ? "o" : "x"}</h2>
-        <h2>Turn Number: {this.state.turnNum}</h2>
->>>>>>> c612210... Added turn number
-=======
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-        <h2>Turn: {this.state.oTurn ? "o" : "x"}</h2>
->>>>>>> cf218b0... Added turn tracking.
-=======
-        <h2>To Play: {this.state.oTurn ? "o" : "x"}</h2>
-        <h2>Turn Number: {this.state.turnNum}</h2>
->>>>>>> c612210... Added turn number
-=======
->>>>>>> e5794e7... Basics done and functionality complete. Square and board finished.
-=======
-        <h2>Turn: {this.state.oTurn ? "o" : "x"}</h2>
->>>>>>> cf218b0... Added turn tracking.
-=======
-        <h2>To Play: {this.state.oTurn ? "o" : "x"}</h2>
-        <h2>Turn Number: {this.state.turnNum}</h2>
->>>>>>> c612210... Added turn number
         <Row>
           <Square val={v[0]} index={0} handleClick={(i, v) => this.handleClick(i, v)}/>
           <Square val={v[1]} index={1} handleClick={(i, v) => this.handleClick(i, v)}/>
